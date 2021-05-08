3634a5d720025db99791601ba370f6becf8d12cf
Nocz <devnocz@gmail.com>
2021-05-07 23:40:03 -0500

    bug fix sys.argv

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ from resources.concrete.handler.argument_handler import SysArgumentHandler
 if __name__ == '__main__':
     try:
         handler = SysArgumentHandler(debug=False)
-        handler.handle_arguments(sys.argv)
+        handler.handle_arguments(*sys.argv)
     except Exception as error:
         raise error
